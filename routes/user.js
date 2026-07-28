357e7895af9d5c5c0170831ca0ffed5ba1a7ee0c
Manas <purnendumanas44@gmail.com>
2026-07-28 19:18:33 +0530

    Implement user login, logout, and navbar updates

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");const passport = require("passport");
+const { redirectUrl } = require("../middleware.js");
 
 
 router.get("/signup", (req, res) =>{
@@ -12,9 +13,13 @@ router.post("/signup", wrapAsync(async (req, res) =>{
     try{let {username, email, password} = req.body;
     const newUser = new User({email, username});
     const registeredUser = await User.register(newUser, password);
-    console.log(registeredUser);
-    req.flash("success", "Welcome to Wanderlust!");
-    res.redirect("/listings");
+    req.login(registeredUser, (err) =>{
+        if (err){
+            return next(err);
+        }
+        req.flash("success", "Welcome to Wanderlust!");
+        res.redirect("/listings");
+    });
     }catch (e){
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -25,9 +30,29 @@ router.get("/login", (req, res)=>{
     res.render("users/login.ejs");
 });
 
-router.post("/login", passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}), async(req, res)=>{
-    req.flash("success", "Welcome back to Wanderlust!")
-    res.redirect("/listings");
+router.post(
+    "/login",
+    redirectUrl,
+    passport.authenticate("local", {
+        failureRedirect: "/login",
+        failureFlash: true
+    }),
+    async (req, res) => {
+        req.flash("success", "Welcome back to Wanderlust!");
+
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
+    }
+);
+
+router.get("/logout", (req, res, next) =>{
+    req.logOut((err) =>{
+        if (err){
+            return next(err);
+        }
+        req.flash("success", "You have been logged Out!");
+        res.redirect("/listings");
+    })
 })
 
 module.exports=router;
